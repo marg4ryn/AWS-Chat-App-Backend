cccad26488ddfb45267613d526f6688ad51e4ae2
marg4ryn <poweski.jan@gmail.com>
2026-04-12 23:38:42 +0200

    User service fix

--- src/main/java/com/example/chatapp/service/UserService.java
+++ src/main/java/com/example/chatapp/service/UserService.java
@@ -1,3 +1,11 @@
+package com.example.chatapp.service;
+
+import com.example.chatapp.entity.User;
+import com.example.chatapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
